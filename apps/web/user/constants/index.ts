35930c09a83647e06d44215df8ff016f5a3670cf
Michael James Urian <businessmjurian@gmail.com>
2025-06-14 17:34:36 +0800

    feat: add HERO_IMAGES constant to index.ts for enhanced hero section content

--- apps/web/user/constants/index.ts
+++ apps/web/user/constants/index.ts
@@ -20,3 +20,41 @@ export const MENU_ITEMS = [
     href: "/contact-us",
   },
 ];
+
+export const HERO_IMAGES = [
+  {
+    src: "/placeholder.svg",
+    alt: "Hero Image 1",
+    title: "Community Hall",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
+  },
+  {
+    src: "/placeholder.svg",
+    alt: "Hero Image 2",
+    title: "Basketball Court",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
+  },
+  {
+    src: "/placeholder.svg",
+    alt: "Hero Image 3",
+    title: "Volleyball Court",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
+  },
+  {
+    src: "/placeholder.svg",
+    alt: "Hero Image 4",
+    title: "Badminton Court",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
+  },
+  {
+    src: "/placeholder.svg",
+    alt: "Hero Image 5",
+    title: "Table Tennis Court",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
+  },
+];
