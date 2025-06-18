ddab017ea75ba260e7f9b29839338da8df35e44c
Michael James Urian <businessmjurian@gmail.com>
2025-06-18 22:29:35 +0800

    style: adjust HeroImage component layout by changing margin for improved spacing and visual appeal

--- apps/web/user/components/main/home/hero/hero-image.tsx
+++ apps/web/user/components/main/home/hero/hero-image.tsx
@@ -151,7 +151,7 @@ function HeroImage() {
           })}
         </AnimatePresence>
       </div>
-      <div className="flex justify-center items-center gap-2 py-4">
+      <div className="flex justify-center items-center gap-2 mt-9 py-4">
         <Button
           variant="ghost"
           size="icon"
