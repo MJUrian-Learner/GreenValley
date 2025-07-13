e23beeac6c2222133ed9717a89f5259ee021def0
Michael James Urian <businessmjurian@gmail.com>
2025-07-13 16:12:55 +0800

    feat: integrate How It Works and CTA components into homepage layout for enhanced user guidance and engagement

--- apps/web/user/app/(main)/page.tsx
+++ apps/web/user/app/(main)/page.tsx
@@ -1,6 +1,8 @@
+import CTA from "@/components/main/home/cta";
 import FAQs from "@/components/main/home/faqs";
 import Features from "@/components/main/home/features";
 import Hero from "@/components/main/home/hero";
+import { HowItWorks } from "@/components/main/home/how-it-works";
 import Promos from "@/components/main/home/promos";
 import Testimonials from "@/components/main/home/testimonials";
 
@@ -9,9 +11,11 @@ export default function Home() {
     <>
       <Hero />
       <Features />
+      <HowItWorks />
       <Testimonials />
       <FAQs />
       <Promos />
+      <CTA />
     </>
   );
 }
